09cf0d15c04ec6ddbc2081d6040746bd6abfbfcf
Joost van der Meijs <j.v.d.meijs@student.tue.nl>
2014-06-30 14:20:53 +0200

    Deleting useless files.

--- Element/element.py
+++
@@ -1,8 +0,0 @@
-#!/bin/python
-class Element:
-	def __init__(self,symbol):
-		self.symbol = symbol
-		file_name = getcwd().elements.txt
-		f = open(file_name,'r')
-		for i in f.readlines():
-			pass
